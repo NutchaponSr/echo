899c97d9253ab3a483bb494273802e7733f9dc68
NutchaponSr <sr.nutchapon@gmail.com>
2025-08-06 15:44:51 +0700

    02: convex package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,22 @@
-import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api"; 
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMeny);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/Web</h1>
-        <Button size="sm">Button</Button>
-        <Input />
-        <p>{add(2, 2)}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>App/Web</p>
+      <Button onClick={() => addUser()}>
+        Add
+      </Button>
+      <pre className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </pre>
     </div>
   )
 }
